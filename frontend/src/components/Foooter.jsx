b44669310d57f88dd4740f5032814a6c7d2190e7
Priyesh Gaur <80889959+Priyesh977@users.noreply.github.com>
2024-02-10 04:31:01 +0530

    Add files via upload
    Nexus Info Final Project Uploaded here.

---
+++ frontend/src/components/Foooter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Foooter() {
+  return (
+    <div className="w-full bg-white ">
+      <footer class="text-center   max-w-7xl mx-auto   text-black lg:text-left">
+        <div class="bg-white flex justify-center items-center gap-3  w-full p-6 text-center ">
+          <span>&copy; 2024 Copyright </span>
+          <h1 className="text-xl    font-bold text-cyan-600 ">
+            PG<span className="text-black">Software</span>
+          </h1>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Foooter;
